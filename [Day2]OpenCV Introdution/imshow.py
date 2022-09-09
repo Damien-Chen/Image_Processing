03b946aafae11cadbd162204ceee787e96f4d189
Damien <81557462+Damien-Chen@users.noreply.github.com>
2022-09-09 23:19:30 +0800

    Update imshow.py

--- [Day2]OpenCV Introdution/imshow.py
+++ [Day2]OpenCV Introdution/imshow.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 
 
-img = cv2.imread("D:\Desktop\IThome\lena.bmp", -1)
+img = cv2.imread("Your File Path", -1)
 cv2.imshow("Example", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
