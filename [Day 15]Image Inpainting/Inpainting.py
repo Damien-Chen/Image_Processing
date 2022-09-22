77ee262cfef50c321881b8233e109cbd08e9e2ef
Damien <81557462+Damien-Chen@users.noreply.github.com>
2022-09-22 21:43:33 +0800

    Add files via upload

---
+++ [Day 15]Image Inpainting/Inpainting.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+def inpainting(f, method = 1):
+    nr, nc = f.shape[:2]
+    mask = np.zeros([nr, nc], dtype = 'uint8')
+    for x in range(nr):
+        for y in range(nc):
+            if f[x, y, 0] == 0 and f[x, y, 1] == 255 and f[x, y, 2] == 255:
+                mask[x, y] = 255
+    
+    if method == 1:
+        g = cv2.inpaint(f, mask, 3, cv2.INPAINT_NS)
+    else:
+        g = cv2.inpaint(f, mask, 3, cv2.INPAINT_TELEA)
+    return g
+
+def main():
+    img1 = cv2.imread("D:\Desktop\IThome\lena.bmp",-1)
+    img2 = inpainting(img1, 1)
+    cv2.imshow("Original", img1)
+    cv2.imshow("Inpainting", img2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+main()
