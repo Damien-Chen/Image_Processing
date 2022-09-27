9416bdbfa3d5565ab7fee7180e6503e83b0b8210
Damien <81557462+Damien-Chen@users.noreply.github.com>
2022-09-27 23:41:17 +0800

    Add files via upload

---
+++ [Day 20] Color Image Filtering/Gaus_filter.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+img1 = cv2.imread("D:\Desktop\IThome\lena.bmp", -1)
+img2 = cv2.GaussianBlur(img1, (5, 5), 0)
+
+cv2.imshow("Original", img1)
+cv2.imshow("After", img2)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+
+
+
